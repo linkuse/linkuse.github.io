3049ff21d5a34e3e68fee9a5a11d43606660d042
linkuse <157085575+linkuse@users.noreply.github.com>
2024-01-23 11:05:12 -0500

    Created main.js v1.0.0

---
+++ main.js
@@ -0,0 +1,13 @@
+function giveLink() {
+	var ins = document.getElementById("oglink").value;
+	const basstr = "https://drive.google.com/uc?id=";
+	const linkid = ins.split("/")[5]
+	document.getElementById("result").innerHTML = basstr + linkid + "&export=download"
+	
+}
+function copyLink() {
+	var ins = document.getElementById("oglink").value;
+	const basstr = "https://drive.google.com/uc?id=";
+	const linkid = ins.split("/")[5]
+	navigator.clipboard.writeText(basstr + linkid + "&export=download")
+}

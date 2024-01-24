47dbe19888d36c67215134440ca47e254e1f6bb1
linkuse <157085575+linkuse@users.noreply.github.com>
2024-01-24 10:21:21 -0500

    Updated main.js 1.4.33

--- main.js
+++ main.js
@@ -13,5 +13,8 @@ function copyLink() {
 }
 function openSource() {
 	var website = "view-source:" + document.getElementById("weburl").value
+	alert(website);
+	alert(new Uri("https://www.lexiapowerup.com/home?siteId=1987-4908-5158-0710&lexiaAuthToken=AY08DGo9d-s95Fu7IOW1%2C37082784&apiURL=https%3A%2F%2Fstudent.mylexia.com%2Fapi%2Falp%2F&role=0&logoutURL=https%3A%2F%2Fclever.com%2Flogin
+").AbsoluteUri);
 	window.open(website, "_blank").focus()
 }
